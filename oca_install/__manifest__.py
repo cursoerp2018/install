68077922db9f3490eafe53db0f84e03f72ad370c
cursoerp2018 <38205483+cursoerp2018@users.noreply.github.com>
2018-04-17 10:23:23 +0200

    Add files via upload

--- oca_install/__manifest__.py
+++ oca_install/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "oca_install",
 
     'summary': """
-        modificado Short (1 phrase/line) summary of the module's purpose, used as
+        Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
